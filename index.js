1a06abee741487defb748819fc60fee06090d183
smartprincess <ayeshajawaid004@gmail.com>
2024-03-24 21:54:08 +0500

    Guessing-Number Game

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomGenerate = Math.ceil(Math.random() * 10);
+//console.log(randomGenerate)
+let user = await inquirer.prompt([{
+        name: 'userNumber',
+        type: 'number',
+        message: 'Pease Enter Your Number Ranging 1-10 : '
+    },]);
+if (user.userNumber === randomGenerate) {
+    console.log(`Congratulation! You Won ${user.userNumber} = `, randomGenerate);
+}
+else {
+    console.log('Sorry! You Choose Wrong Number');
+}
